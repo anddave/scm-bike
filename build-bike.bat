8cc4b90a5b02547cbc429aea680fde66ca4f4d5c
David Hegland <dhegland@fieldglass.com>
2014-08-25 12:28:12 -0500

    bike v1

---
+++ build-bike.bat
@@ -0,0 +1,9 @@
+@del bike.txt
+
+@type handlebar.txt >> bike.txt
+@type seat.txt >> bike.txt
+@type frame.txt >> bike.txt
+@type pedals.txt >> bike.txt
+@type wheels.txt >> bike.txt
+
+@type bike.txt
